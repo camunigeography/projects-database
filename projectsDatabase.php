camunigeography/projects-database

commit 7602796d68edd7117fe18bd45d1ec309c7b933db

Add background color per status in main listing

projectsDatabase.php
@@ -104,10 +104,17 @@ class projectsDatabase extends frontControllerApplication
 		$html = '';
 		
 		# Get the list of projects
-		if (!$projects = $this->databaseConnection->select ($this->settings['database'], $this->settings['table'], $conditions = "status != 'proposed'", $columns = array (), true, $orderBy = "FIELD(status, 'developing','additional','specced','proposed','completed'), id DESC")) {
+		if (!$projectsRaw = $this->databaseConnection->select ($this->settings['database'], $this->settings['table'], $conditions = "status != 'proposed'", $columns = array (), true, $orderBy = "FIELD(status, 'developing','additional','specced','proposed','completed'), id DESC")) {
 			$html = "\n<p>There are no confirmed projects at present.</p>";
 			echo $html;
 			return;
+		}
+		
+		# Attach status string to the key for CSS styling purposes
+		$projects = array ();
+		foreach ($projectsRaw as $id => $project) {
+			$key = $id . ' ' . lcfirst ($project['status']);
+			$projects[$key] = $project;
 		}
 		
 		# Assemble fields
@@ -134,7 +141,7 @@ class projectsDatabase extends frontControllerApplication
 		$html .= "\n<p>You can <a href=\"{$this->baseUrl}/add.html\">request a project</a>.</p>";
 		$tableHeadingSubstitutions = $this->databaseConnection->getHeadings ($this->settings['database'], $this->settings['table']);
 		$html .= "\n" . '<!-- Enable table sortability: --><script language="javascript" type="text/javascript" src="/sitetech/sorttable.js"></script>';
-		$html .= application::htmlTable ($projects, $tableHeadingSubstitutions, 'lines sortable" id="sortable', $keyAsFirstColumn = false, false, $allowHtml);
+		$html .= application::htmlTable ($projects, $tableHeadingSubstitutions, 'lines sortable" id="sortable', $keyAsFirstColumn = false, false, $allowHtml, false, false, $addRowKeyClasses = true);
 		
 		# Show the HTML
 		echo $html;

styles.css
@@ -1,1 +1,4 @@
 #projectsdatabase th {background-color: #f7f7f7;}
+#projectsdatabase table tr.completed td {background-color: #d1f7cc;}
+#projectsdatabase table tr.specced td {background-color: #fbffaa;}
+#projectsdatabase table tr.developing td {background-color: #ffcc99;}

commit a8e79b1050c2cff73bd78ae07458f3bbe92cb950

Fix link to record in listing caused by regression in 7602796d68edd7117fe18bd45d1ec309c7b933db

projectsDatabase.php
@@ -120,7 +120,7 @@ class projectsDatabase extends frontControllerApplication
 		# Assemble fields
 		foreach ($projects as $id => $project) {
 			if ($this->userIsAdministrator) {
-				$projects[$id]['id'] = "<a href=\"{$this->baseUrl}/projects/{$id}/edit.html\"><strong>" . $projects[$id]['id'] . '</strong></a>';
+				$projects[$id]['id'] = "<a href=\"{$this->baseUrl}/projects/{$project['id']}/edit.html\"><strong>" . $projects[$id]['id'] . '</strong></a>';
 			}
 			$projects[$id]['name'] = "<a href=\"{$projects[$id]['url']}\" target=\"_blank\">" . htmlspecialchars ($projects[$id]['name']) . '</a>';
 			unset ($projects[$id]['url']);